49fa2d3516efc58afbc8bc60509f5c17c49d109d
siites <siites@gmail.com>
2020-06-25 23:32:15 +0900

    blog Blueprint

---
+++ README.md
@@ -0,0 +1,6 @@
+```
+# export FLASK_APP=app  
+# export FLASK_ENV=development  
+# flask init-db  
+# flask run
+```
--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY=b'\xb4\xbb\x80\xb29|\x03!<chK\x8d\xe0@\xfe',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
@@ -23,10 +23,18 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    @app.route('/hello')
+    def hello_world():
+        return 'Hello World!'
+
     from . import db
     db.init_app(app)
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
